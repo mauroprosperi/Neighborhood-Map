846ee319bc7ce2fb65c9f6afddaefd7cd557d2c6
Mauro <mauroseba98@hotmail.com>
2018-10-26 15:47:40 -0300

    locations test added

---
+++ js/locations.js
@@ -0,0 +1,22 @@
+var locations = [
+    {
+        title: 'Loco Al Pollo',
+        lat: -32.911912,
+        lng: -68.860137
+    },
+    {
+        title: 'Tia Rasca',
+        lat: -32.912898,
+        lng: -68.860165
+    },
+    {
+        title: 'La granja de los Quesos',
+        lat: -32.914685,
+        lng: -68.860201
+    },
+
+    {title: 'La casa del IONI',
+     lat: -32.908116,
+     lng: -68.859394
+    }
+   ];
