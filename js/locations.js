e21d2e547240341fb924c782aedef8ad0303af54
Mauro <mauroseba98@hotmail.com>
2018-10-31 18:56:35 -0300

    locations modify to a old object version

--- js/locations.js
+++ js/locations.js
@@ -1,22 +1,19 @@
 var locations = [
     {
         title: 'Loco Al Pollo',
-        lat: -32.911912,
-        lng: -68.860137
+        location: {lat: -32.911912, lng: -68.860137}
     },
     {
         title: 'Tia Rasca',
-        lat: -32.912898,
-        lng: -68.860165
+        location: {lat: -32.912898, lng: -68.860165}
     },
     {
         title: 'La granja de los Quesos',
-        lat: -32.914685,
-        lng: -68.860201
+        location: {lat: -32.914685, lng: -68.860201}
     },
 
-    {title: 'La casa del IONI',
-     lat: -32.908116,
-     lng: -68.859394
+    {
+        title: 'La casa del IONI',
+        location: {lat: -32.908116, lng: -68.859394}
     }
    ];
