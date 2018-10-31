6c185f5bd235a11ff1170dcd233d7c1bd786f031
Mauro <mauroseba98@hotmail.com>
2018-10-31 19:05:46 -0300

    better call of lat/lng of the marker to populate the info window

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var map, clientId, clientSecretId
+var map, clientId, clientSecretId;
 
 function viewModel() {
     var self = this;
     this.markers = [];
-
+    this.searchOption = ko.observable("");
 
     this.populateInfoWindow = function(marker, infowindow) {
         //Check to make sure the infowindow is not already opened on this marker.
@@ -11,38 +11,44 @@ function viewModel() {
           //Clear the infowindow content to give the streetview time to load.
           infowindow.setContent('');
           infowindow.marker = marker;
-          //Forsquare API
+          //I research through the documentation and find the getPosition method, this returns a LatLng class!
+          var lat = marker.getPosition().lat();
+          var lng = marker.getPosition().lng();
+          //Forsquare API 
           clientId = 'YJF01YJ145HRMOEXPWXZQ10KXZB0KBUXIKNEI1NS4SEFQFGK';
           clientSecretId = 'UAHG4CEA5NI0CQGNT0BECNFTWXKQMWHEQLDVF5TZWNEN0XHH';
-          var url = 'https://api.foursquare.com/v2/venues/search?ll=' + marker.lat + ',' +
-                marker.lng + '&client_id=' + clientId + '&client_secret=' +
-                clientSecretId + '&query=' + marker.title + '&v=20170708' + '&m=forsquare';
+          var url = 'https://api.foursquare.com/v2/venues/search?ll=' + lat + ',' +
+                lng + '&client_id=' + clientId + '&client_secret=' +
+                clientSecretId + '&query=' + marker.title + '&v=20170708' + '&m=foursquare';
 
-          $.getJSON(url).done(function(marker) {
-            var response = marker.response.venues[0];
-            self.city = response.location.formattedAddress[1];
-            self.zip = response.location.formattedAddress[3];
-            self.country = response.location.formattedAddress[4];
-
-            self.responseContent =
-                '<div>' +
-                    '<h6 class="iw_address_title"> Address: </h6>' +
-                    '<p class="iw_address">' + self.city + '</p>' +
-                    '<p class="iw_address">' + self.zip + '</p>' +
-                    '<p class="iw_address">' + self.country + '</p>' +
-                '</div>' +
-            '</div>';   // el div se abre mas abajo en el content del html,
-                        // revisar para ver si se puede eliminar aca
-            infowindow.setContent(self.markupContent + self.responseContent);
-          }).fail(function(){
-              alert(" there was an issue loading the api, please killyourself");
+            $.getJSON(url).done(function(marker) {
+                var response = marker.response.venues[0];
+                self.street = response.location.formattedAddress[0];
+                self.city = response.location.formattedAddress[1];
+                self.zip = response.location.formattedAddress[3];
+                self.country = response.location.formattedAddress[4];
+                self.category = response.categories[0].shortName;
+                // htmlContentFoursquare
+                self.htmlContentFoursquare = '<div>';
+                if (self.category) {
+                	self.htmlContentFoursquare += '<h5 class="iw_subtitle">('+ self.category +')</h5>'
+                }
+                if (self.street) {
+                	self.htmlContentFoursquare += '<h6 class="iw_address_title"> Address: </h6> <p class="iw_address">' + self.street + '</p>'
+                }
+                if (self.city) {
+                	self.htmlContentFoursquare += '<p class="iw_address">' + self.city + '</p>'
+                }
+                if (self.zip) {
+                    self.htmlContentFoursquare += '<p class="iw_address">' + self.zip + '</p>  </div>'
+                }
+                infowindow.setContent(self.markupContent + self.htmlContentFoursquare);
+            }).fail(function(){
+              alert(" an error was found loading the API");
           });
-          // aca se abre el div
-          this.markupContent = '<div>' + '<h4 class="iw_title">' + marker.title + '</h4>';
-
+          this.markupContent = '<h4 class="iw_title">' + marker.title + '</h4>';
           infowindow.open(map, marker);
-                            // CLOSECLICK FTW 
-          infowindow.addListener('closeclick', function(){
+          infowindow.addListener('click', function(){
               infowindow.marker = null;
           });
         }
@@ -50,14 +56,14 @@ function viewModel() {
 
 
     this.markerPopulate = function() {
-        self.populateInfoWindow(this, self.largeinfowindow); // cambiar nobmre ?
-        this.setAnimation(google.maps.Animation.BOUNCE); // asd 
+        self.populateInfoWindow(this, self.infoWindowMaps);
+        this.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout((function() {
             this.setAnimation(null);
         }).bind(this), 1400);
     };
 
-    initMap = function() {
+    this.initMap = function() {
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: -32.912951, lng: -68.862329},
             zoom: 15,
@@ -70,18 +76,12 @@ function viewModel() {
     this.infoWindowMaps = new google.maps.InfoWindow();
     for ( var i = 0; i < locations.length; i++) {
         this.title = locations[i].title;
-        this.lat = locations[i].lat;
-        this.lng = locations[i].lng;
+        this.position = locations[i].location;
 
         this.googleMarker = new google.maps.Marker({
             map: map,
-            position: {
-                lat:  this.lat,
-                lng:  this.lng
-            },
+            position: this.position,
             title: this.title,
-            lat: this.lat,
-            lng: this.lng,
             id: i,
             animation: google.maps.Animation.DROP
         });
@@ -112,7 +112,7 @@ function viewModel() {
 // cambiar giladas
 googleError = function googleError() {
     alert(
-        'Oops. Google Maps did not load. Please refresh the page and try again!'
+        'Google Maps did not load. Please refresh the page or try again later'
     );
 };
 
